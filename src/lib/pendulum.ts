db2aaa8565263afe8241e43656c57529c40eee55
Super Yoshi 10000 <SuperYoshi10000@gmail.com>
2025-11-09 18:18:42 -0500

    add equations

--- src/lib/pendulum.ts
+++ src/lib/pendulum.ts
@@ -7,15 +7,31 @@ export interface PendulumState extends Vector2 {
     time: number;
 }
 
+export class View implements Vector2 {
+    x: number = 0;
+    y: number = 0;
+    scale: number = 100;
+    constructor() {}
+
+    calcPosXY(pos: Vector2): [number, number] {
+        return [
+            (this.x + pos.x) * this.scale,
+            (this.y + pos.y) * this.scale
+        ];
+    }
+}
+
 export class PendulumPart implements Vector2 {
+    static readonly DEFAULT_ANGLE = Math.PI / 2;
+
     length: number = 1;
     mass: number = 1;
     
-    startAngle: number = 90;
+    startAngle: number = PendulumPart.DEFAULT_ANGLE;
     startX: number  = 0;
     startY: number = -1;
 
-    angle: number = 90;
+    angle: number = PendulumPart.DEFAULT_ANGLE;
     x: number = 0;
     y: number = -1;
 
@@ -25,11 +41,14 @@ export class PendulumPart implements Vector2 {
     
     previousPositions: PendulumState[] = [];
 
-    next(dt: number) {}
+    next(dt: number, acceleration: number) {
+        this.velocity += acceleration * dt;
+        this.angle += this.velocity * dt;
+    }
 
 
 
-    draw(ctx: CanvasRenderingContext2D, time: number, parentPos: Vector2) {
+    draw(ctx: CanvasRenderingContext2D, time: number, parentPos: Vector2, view: View) {
         const drawFadeTime = this.display.drawFadeTime || 0;
         
         // Remove old positions
@@ -49,8 +68,8 @@ export class PendulumPart implements Vector2 {
         }
         ctx.globalAlpha = 1;        
 
-        this.display.drawPoint(ctx, this.x, this.y); // Ball
-        this.display.drawLine(ctx, parentPos.x, parentPos.y, this.x, this.y); // Rod
+        this.display.drawPoint(ctx, this, view); // Ball
+        this.display.drawLine(ctx, parentPos, this, view); // Rod
     }
 
 }
@@ -59,14 +78,62 @@ export class PendulumPart implements Vector2 {
 export class Pendulum {
     gravity: number = 9.81;
     position: Vector2 = { x: 0, y: 0 };
+    view: View = new View();
     rootDisplay: PendulumDisplay = new PendulumDisplay();
     parts: PendulumPart[] = [];
+    readonly count: number;
 
-    constructor() {}
+    constructor(count: "single" | "double" | number = "double") {
+        if (count === "single") this.count = 1;
+        else if (count === "double") this.count = 2;
+        else this.count = count;
+    }
 
     next(dt: number) {
-        this.angle += this.angularVelocity * dt;
-        // I have no idea how pendulums work so I'll do this later
+        if (this.count === 1) {}
+        if (this.count === 2) {
+            // Note: t (theta) = angle (because a is used for acceleration)
+
+            const t1 = this.parts[0].angle;
+            const m1 = this.parts[0].mass;
+            const l1 = this.parts[0].length;
+            const v1sq = this.parts[0].velocity ** 2;
+            const sin1 = Math.sin(t1);
+
+            const t2 = this.parts[1].angle;
+            const m2 = this.parts[1].mass;
+            const l2 = this.parts[1].length;
+            const v2sq = this.parts[1].velocity ** 2;
+            const sin2 = Math.sin(t2);
+            
+
+            const delta = t2 - t1;
+            const sind = Math.sin(delta);
+            const cosd = Math.cos(delta);
+            const sincosd = sind * cosd;
+            
+            const massSum = m1 + m2;
+            const massTimesGravity = massSum * this.gravity;
+            
+            const d1 = massSum * l1 - m2 * l1 * (cosd ** 2);
+            const d2 = (l2 / l1) * d1;
+
+            const a1 = (
+                m2 * l1 * v1sq * sincosd
+                + m2 * this.gravity * sin2 * cosd
+                + m2 * l2 * v2sq * sind
+                - massTimesGravity * sin1
+            ) / d1;
+            const a2 = (
+                -m2 * l2 * v2sq * sincosd
+                + massTimesGravity * sin1 * cosd
+                - massSum * l1 * v1sq * sind
+                - massTimesGravity * sin2
+            ) / d2;
+            
+            this.parts[0].next(dt, a1);
+            this.parts[1].next(dt, a2);
+        }
     }
 
 }
@@ -109,22 +176,22 @@ export class PendulumDisplay {
         this.drawFadeTime = options.drawFadeTime || 0;
     }
 
-    drawLine(ctx: CanvasRenderingContext2D, x1: number, y1: number, x2: number, y2: number) {
+    drawLine(ctx: CanvasRenderingContext2D, parentPos: Vector2, pos: Vector2, view: View) {
         ctx.strokeStyle = this.lineColor;
         ctx.lineWidth = this.lineWidth;
         ctx.beginPath();
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
+        ctx.moveTo(...view.calcPosXY(parentPos));
+        ctx.lineTo(...view.calcPosXY(pos));
         ctx.stroke();
     }
 
-    drawPoint(ctx: CanvasRenderingContext2D, x: number, y: number) {
+    drawPoint(ctx: CanvasRenderingContext2D, pos: Vector2, view: View) {
         ctx.fillStyle = this.color;
         ctx.strokeStyle = this.borderColor;
         ctx.lineWidth = this.borderWidth;
         const radius = this.size / 2;
         ctx.beginPath();
-        ctx.ellipse(x, y, radius, radius, 0, 0, Math.PI * 2);
+        ctx.ellipse(...view.calcPosXY(pos), radius, radius, 0, 0, Math.PI * 2);
         if (this.borderFirst) {
             ctx.stroke();
             ctx.fill();
